2eea09163f2371acb716f6e68fc0f651afb6ac2b
Jlw2uc <32882560+Jlw2uc@users.noreply.github.com>
2017-12-19 18:36:11 -0500

    Add files via upload

--- final_project/js/main.js
+++ final_project/js/main.js
@@ -1,2 +1,47 @@
-// when the photos in the gallery are clicked
-//pop up a modal window with a larger version and information
+
+//display error message on Form
+$('form').on('submit', function (event) {
+  event.preventDefault();
+
+ var fullName = $('#name').val();
+ var emailAddress = $('#email').val();
+
+ if (fullName.length <= 1){
+   $('#name').addClass('error');
+   $('#nameerror').fadeIn('800');
+ } else {
+   $('#name').removeClass('error');
+   $('#nameerror').fadeOut('800');
+ }
+ if (emailAddress.length <= 1){
+   $('#email').addClass('error');
+   $('#emailerror').fadeIn('800');
+ } else {
+   $('#email').removeClass('error');
+   $('#emailerror').fadeOut('800');
+ }
+
+});
+
+//make a scrolled nav with if else statements
+$(window).on('scroll', function () {
+	// Step 1: Google $(window).scrollTop();
+	var distanceScrolled = $(window).scrollTop();
+
+	if (distanceScrolled > 40) {
+		$('nav').addClass('scrolled');
+	} else {
+		$('nav').removeClass('scrolled');
+	}
+
+});
+
+// make a hamburger menu that only shows with mobile devices
+$('#hamburger').on('click', function(){
+  $('.nav-links').addClass('menu-open');
+
+});
+
+$('#close').on('click', function(){
+  $('.nav-links').removeClass('menu-open');
+});
